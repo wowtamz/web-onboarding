dd85ad678a5a76aa2f10ca67dbb21a07eb42b17e
Vincent <vincstei@gmail.com>
2024-06-17 13:55:30 +0200

    AssigmentTemplate: Model, Container, ViewModel fertig

---
+++ Projekt/SoPro24Team06/Models/AssigmentTemplateContainer.cs
@@ -0,0 +1,60 @@
+using SoPro24Team06.Models;
+using SoPro24Team06.Data;
+using System.Collections.Generic;
+
+public class AssigmentTemplateContainer
+{
+    private Context db = new();
+
+    public void AddAssigmentTemplate(string title, string? instructions, DueTime dueIn, List<Department>? forDepartmentsList, List<Contract>? forContractsList, AssigneType assigneType, List<Role>? assignedRoles)
+    {
+        db.Add(new AssigmentTemplate{Title = title, Instructions = instructions, DueIn = dueIn, ForDepartmentList = forDepartmentList, ForContractList = forContractList, AssigneType = assigneType, AssignedRoles = assignedRoles});
+        db.SaveChanges();
+    }
+    public void DeleteAssigmentTemplate(int id)
+    {
+        if(db.AssigmentTemplates != null){
+            AssigmentTemplate? assigmentTemplate= db.AssigmentTemplates.FirstOrDefault(assigmentTemplate => assigmentTemplate.id == id);
+            if(assigmentTemplate != null){
+                db.Remove(assigmentTemplate!);
+                db.SaveChanges();
+            }
+        }
+    }
+    public void EditAssigmentTemplate(string title, string? instructions, DueTime dueIn, List<Department>? forDepartmentsList, List<Contract>? forContractsList, AssigneeType assigneeType, List<Role>? assignedRoles)
+    {
+        if(db.AssigmentTemplates != null){
+            AssigmentTemplate? assigmentTemplate = db.AssigmentTemplates.FirstOrDefault(assigmentTemplate => assigmentTemplate.id == id);
+            if(assigmentTemplate != null){
+                assigmentTemplate.Title = title;
+                assigmentTemplate.Instructions = instructions;
+                assigmentTemplate.DueIn = dueIn;
+                assigmentTemplate.ForDepartmentsList = forDepartmentsList;
+                assigmentTemplate.ForContractsList = forContractsList;
+                assigmentTemplate.AssigneeType = assigneeType;
+                assigmentTemplate.AssignedRoles = assignedRoles;
+                db.Update(assigmentTemplate);
+                db.SaveChanges();
+            }
+        }
+    }
+    public AssigmentTemplate GetAssigmentTemplate(int id)
+    {
+         if(db.AssigmentTemplates != null){
+            AssigmentTemplate? assigmentTemplate = db.AssigmentTemplates.FirstOrDefault(assigmentTemplate => assigmentTemplate.id == id);
+            if(assigmentTemplate != null){
+                return assigmentTemplate!;
+            }
+        }
+        return null;
+    }
+
+     public List<AssigmentTemplate> GetAllAssigmentTemplate()
+    {
+        List<AssigmentTemplate> assigmentTemplateList = new List<AssigmentTemplate>();
+        if(db.AssigmentTemplates != null){
+            lessons = db.AssigmentTemplates.ToList();
+        }
+        return assigmentTemplateList;
+    }
+}
