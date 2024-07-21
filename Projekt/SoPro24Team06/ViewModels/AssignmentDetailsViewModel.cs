244f0f5124dfe1a54417ba0d1825a35b4babc7d8
Jan Pfluger <jan.pfluger@uni-a.de>
2024-07-21 20:45:12 +0200

    modified:   Controllers/AssignmentController.cs 	modified:   ViewModels/AssignmentDetailsViewModel.cs 	modified:   Views/Assignments/AssignmentDetails.cshtml

--- Projekt/SoPro24Team06/ViewModels/AssignmentDetailsViewModel.cs
+++ Projekt/SoPro24Team06/ViewModels/AssignmentDetailsViewModel.cs
@@ -8,11 +8,13 @@ namespace SoPro24Team06.ViewModels
 {
     public class AssignmentDetailsViewModel
     {
-        public Assignment  Assignment { get; set; }
-        public SelectList  UserList { get; set; }
-        public SelectList  RoleList { get; set; }
-        public Process ? Process { get; set; }
-        public SelectList  AssignmentStatusList { get; set; }
+        public Assignment Assignment { get; set; }
+        public SelectList UserList { get; set; }
+        public SelectList RoleList { get; set; }
+        public Process? Process { get; set; }
+        public SelectList AssignmentStatusList { get; set; }
+
+		public SelectList AssigneeTypeList {get; set;}
 
         public AssignmentDetailsViewModel(
             Assignment assignment,
@@ -22,32 +24,52 @@ namespace SoPro24Team06.ViewModels
         )
         {
             this.Assignment = assignment;
-			if (this.Assignment.AssigneeType == AssigneeType.USER && this.Assignment.Assignee != null)
-			{
-				this.UserList = new SelectList(userList, "Id", "FullName", this.Assignment.Assignee);
-			}
-			else
-			{
-				this.UserList = new SelectList(userList, "Id", "FullName");
-			}
-			if (this.Assignment.AssigneeType == AssigneeType.ROLES && this.Assignment.AssignedRole != null)
-			{
-				this.RoleList = new SelectList(roleList, "Id", "Name", this.Assignment.AssignedRole);
-			}
-			else
-			{
-				this.RoleList = new SelectList(roleList, "Id", "Name");
-			}
+            if (
+                this.Assignment.AssigneeType == AssigneeType.USER
+                && this.Assignment.Assignee != null
+            )
+            {
+                this.UserList = new SelectList(
+                    userList,
+                    "Id",
+                    "FullName",
+                    this.Assignment.Assignee
+                );
+            }
+            else
+            {
+                this.UserList = new SelectList(userList, "Id", "FullName");
+            }
+            if (
+                this.Assignment.AssigneeType == AssigneeType.ROLES
+                && this.Assignment.AssignedRole != null
+            )
+            {
+                this.RoleList = new SelectList(
+                    roleList,
+                    "Id",
+                    "Name",
+                    this.Assignment.AssignedRole
+                );
+            }
+            else
+            {
+                this.RoleList = new SelectList(roleList, "Id", "Name");
+            }
             this.Process = process;
             this.AssignmentStatusList = new SelectList(
                 EnumHelper.GetEnumList<AssignmentStatus>(),
                 "Value",
                 "Text",
-				Assignment.Status
+                Assignment.Status
             );
+			
+			List<AssigneeType> assingeeTypeListTemp = new List<AssigneeType> {AssigneeType.ROLES, AssigneeType.USER};
+			AssigneeTypeList = new SelectList(assingeeTypeListTemp.Select(e => new {Value = e, Text = EnumHelper.GetDisplayName(e)}), "Value", "Text", Assignment.AssigneeType);
+				
         }
 
-		public AssignmentDetailsViewModel() {}
+        public AssignmentDetailsViewModel() { }
     }
 }
 //end codeownership Jan Pfluger
