601aa3b0be4b8d416fea948ed0574373ba1eb16d
Jan Pfluger <jan.pfluger@uni-a.de>
2024-07-26 13:10:35 +0200

    new file:   ../SoPro24Team06.E2E/AssignmentUiTest.cs

---
+++ Projekt/SoPro24Team06.E2E/AssignmentUiTest.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+
+namespace SoPro24Team06.E2E
+{
+	public class AssignmentUiTest
+	{
+		private readonly IWebDriver _driver;
+		private readonly WebDriverWait _wait;
+	
+		public AssignmentUiTest(IWebDriver driver, WebDriverWait wait)
+		{
+			_driver = driver
+			_wait = wait
+		}
+
+		public void Login()
+	}
+}
+
