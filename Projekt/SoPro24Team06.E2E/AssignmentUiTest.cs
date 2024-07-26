fd5efa3379ff0760c177c2d46b426e58658f4c97
Jan Pfluger <jan.pfluger@uni-a.de>
2024-07-26 13:17:13 +0200

    modified:   ../SoPro24Team06.E2E/AssignmentUiTest.cs 	modified:   Controllers/AssignmentController.cs

--- Projekt/SoPro24Team06.E2E/AssignmentUiTest.cs
+++ Projekt/SoPro24Team06.E2E/AssignmentUiTest.cs
@@ -3,21 +3,19 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Xunit;
 
-
 namespace SoPro24Team06.E2E
 {
-	public class AssignmentUiTest
-	{
-		private readonly IWebDriver _driver;
-		private readonly WebDriverWait _wait;
-	
-		public AssignmentUiTest(IWebDriver driver, WebDriverWait wait)
-		{
-			_driver = driver
-			_wait = wait
-		}
+    public class AssignmentUiTest
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
 
-		public void Login()
-	}
+        public AssignmentUiTest(IWebDriver driver, WebDriverWait wait)
+        {
+            _driver = driver;
+            _wait = wait;
+        }
+
+        public void Login() { }
+    }
 }
-
