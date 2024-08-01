bdb7e850a67a02d11e324d0909e26aae2959dbb3
Michael Adolf <michael.adolf@uni-a.de>
2024-08-01 22:36:13 +0200

    added description

--- Projekt/SoPro24Team06/Areas/Identity/Pages/Account/Lockout.cshtml.cs
+++ Projekt/SoPro24Team06/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SoPro24Team06.Areas.Identity.Pages.Account
 {
+    /// <summary>
+    /// Model for the Lockout page
+    /// </summary>
     public class LockoutModel : PageModel
     {
         public void OnGet()
