3dd232ae41f1e568e024c7b39d292863d83176ed
Michael Adolf <michael.adolf@uni-a.de>
2024-08-01 22:30:51 +0200

    added Lockout controller

---
+++ Projekt/SoPro24Team06/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoPro24Team06.Areas.Identity.Pages.Account
+{
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
