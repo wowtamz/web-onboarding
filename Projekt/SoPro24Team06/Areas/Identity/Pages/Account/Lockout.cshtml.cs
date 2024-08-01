e013df09beb7cd5733a1b743d18bf8f9c5d413f1
Michael Adolf <michael.adolf@uni-a.de>
2024-08-01 22:35:27 +0200

    code ownership

--- Projekt/SoPro24Team06/Areas/Identity/Pages/Account/Lockout.cshtml.cs
+++ Projekt/SoPro24Team06/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+//-------------------------
+// Author: Michael Adolf
+//-------------------------
+
 namespace SoPro24Team06.Areas.Identity.Pages.Account
 {
     public class LockoutModel : PageModel
