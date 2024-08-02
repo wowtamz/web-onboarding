b952c5614fbb97695dac234dd7e7fd1ca5f337a3
Michael Adolf <michael.adolf@uni-a.de>
2024-08-02 23:59:14 +0200

    added password validation options

--- Projekt/SoPro24Team06/ViewModels/CreateUserViewModel.cs
+++ Projekt/SoPro24Team06/ViewModels/CreateUserViewModel.cs
@@ -30,6 +30,7 @@ namespace SoPro24Team06.ViewModels
 
         [Required(ErrorMessage = "Passwort ist erforderlich")]
         [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "Das {0} muss mindestens {2} Zeichen lang sein.", MinimumLength = 6)]
         [Display(Name = "Passwort")]
         public string Password { get; set; } = string.Empty;
 
