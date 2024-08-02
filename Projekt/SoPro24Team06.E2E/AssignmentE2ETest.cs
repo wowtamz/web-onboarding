15bb459ac92268dd6966299abe2120fc504ddff2
Jan Pfluger <jan.pfluger@uni-a.de>
2024-08-02 20:58:42 +0200

    modified:   SoPro24Team06.E2E/AssignmentE2ETest.cs 	modified:   ../docs/How To E2E Test with WebApplicationFactory.md

--- Projekt/SoPro24Team06.E2E/AssignmentE2ETest.cs
+++ Projekt/SoPro24Team06.E2E/AssignmentE2ETest.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SoPro24Team06.Data;
 using SoPro24Team06.Models;
 
 namespace SoPro24Team06.E2E
@@ -14,6 +16,8 @@ namespace SoPro24Team06.E2E
         private readonly CustomWebApplicationFactory<Program> _factory;
         private readonly ChromeDriver _driver;
         private readonly WebDriverWait _wait;
+        private readonly string _errorLocationClass = "AssignmentE2ETest";
+        private readonly Uri baseUrl = new Uri("https://localhost:/7003/");
 
         public AssignmentE2ETest()
         {
@@ -34,45 +38,158 @@ namespace SoPro24Team06.E2E
         }
 
         [Fact]
-        public async Task TestIfIdentityWorks()
+        public async Task AssignmentListTest()
         {
+            string errorLocationFunktion = "AssignmentListTest";
+            //start Client
             HttpClient client = _factory.CreateDefaultClient();
-            //await TestIfIsRunning(client);
-            string userName = "testuser@example.com";
-            string password = "Password123!";
+
+            List<Assignment> assignments = new List<Assignment>();
+            ApplicationUser user;
+            //using scoped services to create only neccecary Data in Database
             using (var scope = _factory.Services.CreateScope())
             {
-                // Resolve the UserManager and other services from the scope
+                // Resolve the UserManager Role Manager and context from the scope
                 var userManager = scope.ServiceProvider.GetRequiredService<
                     UserManager<ApplicationUser>
                 >();
                 var roleManager = scope.ServiceProvider.GetRequiredService<
                     RoleManager<ApplicationRole>
                 >();
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-                // Your test logic here
-                var user = new ApplicationUser
-                {
-                    FullName = "Test User",
-                    UserName = userName,
-                    Email = userName
-                };
-                var result = await userManager.CreateAsync(user, password);
-                userManager.AddToRoleAsync(user, "Administrator");
+                await SetTestData(context, userManager, roleManager);
+                user = await userManager.FindByEmailAsync("user@example.com");
+            }
+            if (user == null)
+                throw new Exception("no user found");
+            //login as Normal User
+            await Login("user@example.com", "User@123");
 
-                if (!result.Succeeded)
+            //check Assignment List:
+            _driver
+                .Navigate()
+                .GoToUrl("https://localhost:7003/Assignment/ChangeTable?currentList=MyAssignments");
+
+            //test if AssignmentList is displayed
+            IWebElement assignmentListBody = _wait.Until(d => d.FindElement(By.TagName("tbody")));
+            try
+            {
+                Assert.True(
+                    assignmentListBody.Displayed,
+                    "assignmentList myAssignmentsList not displayed"
+                );
+            }
+            catch (Exception e)
+            {
+                throw new Exception(
+                    ""
+                        + _errorLocationClass
+                        + errorLocationFunktion
+                        + "AssignmentList-isDisplayed-Check: "
+                        + e.Message
+                );
+            }
+
+            //check if any Assignments are Displayed
+            try
+            {
+                assignmentListBody.FindElement(
+                    By.XPath("//tr/td[text()='Keine Aufgaben vorhanden']")
+                );
+
+                throw new Exception(
+                    ""
+                        + _errorLocationClass
+                        + errorLocationFunktion
+                        + "AssignmentList-noAssignments-Check: noAssignmentsFound"
+                );
+            }
+            catch (Exception e) { }
+
+            //filter assignments for User
+            List<Assignment> assignmentsForUser = assignments
+                .Where(a => a.Assignee != null && a.Assignee == user)
+                .ToList();
+
+            //check number of Displayed Elements:
+            List<IWebElement> assignmentListContent = assignmentListBody
+                .FindElements(By.XPath("/tr"))
+                .ToList();
+            if (assignmentListContent.Count() != assignmentsForUser.Count)
+            {
+                throw new Exception(
+                    ""
+                        + _errorLocationClass
+                        + errorLocationFunktion
+                        + "AssignmentList-NumberOfAssignments-Check: is not correct"
+                );
+            }
+
+            //check content of Assignments
+            foreach (var a in assignmentListContent)
+            {
+                IWebElement TitleElement = a.FindElement(By.XPath("/td[1]"));
+                string assignmentTitle = TitleElement.Text;
+                //check if Assignment is actually in the List
+                Assignment? assignmentToCheck = assignmentsForUser.Find(a =>
+                    a.Title == assignmentTitle
+                );
+                if (assignmentToCheck != null)
                 {
-                    throw new InvalidOperationException(
-                        $"Failed to create test user: {string.Join(", ", result.Errors.Select(e => e.Description))}"
+                    //check if Assignment is overdue
+                    if (assignmentToCheck.DueDate.CompareTo(DateTime.Today) < 0)
+                    {
+                        if (a.GetAttribute("class") != ".overdue")
+                        {
+                            throw new Exception(
+                                ""
+                                    + _errorLocationClass
+                                    + errorLocationFunktion
+                                    + "AssignmentList-CheckListContent: overDue Assignment not Displayed Correctly"
+                            );
+                        }
+                    }
+
+                    //check if DaysTillDueDate are displayed Correctly:
+                    if (
+                        a.FindElement(By.XPath("/td[3]")).Text
+                        != assignmentToCheck.GetDaysTillDueDate().ToString()
+                    )
+                    {
+                        throw new Exception(
+                            ""
+                                + _errorLocationClass
+                                + errorLocationFunktion
+                                + "AssignmentList-CheckListContent: Days Till DueDate not displayed correctly"
+                        );
+                    }
+
+                    assignmentsForUser.Remove(assignmentToCheck);
+                }
+                else
+                {
+                    throw new Exception(
+                        ""
+                            + _errorLocationClass
+                            + errorLocationFunktion
+                            + "AssignmentList-CheckListContent: An Assignment was not found in the List for the current User"
                     );
                 }
-                Assert.True(result.Succeeded);
+
+                if (assignmentsForUser.Count() != 0)
+                {
+                    throw new Exception(
+                        ""
+                            + _errorLocationClass
+                            + errorLocationFunktion
+                            + "AssignmentList-CheckListContent: not all Assignments from the Database where Displayed"
+                    );
+                }
             }
-            //Login("admin@gmx.de", "Admin1!");
-            Login(userName, password);
         }
 
-        public void Login(string userName, string password)
+        public async Task Login(string userName, string password)
         {
             _driver.Navigate().GoToUrl("https://localhost:7003/");
 
@@ -93,7 +210,6 @@ namespace SoPro24Team06.E2E
                     {
                         throw new Exception("Failed to find userName Field");
                     }
-                    Task.Delay(2000);
 
                     try
                     {
@@ -108,7 +224,6 @@ namespace SoPro24Team06.E2E
                     {
                         throw new Exception("Failed to find Password Field");
                     }
-                    Task.Delay(2000);
 
                     IWebElement loginButton;
                     try
@@ -143,7 +258,7 @@ namespace SoPro24Team06.E2E
                     {
                         throw new Exception("Failed to click login button" + e.Message);
                     }
-                    Task.Delay(20000);
+
                     if (_driver.Url.Contains("Identity/Account/Login"))
                     {
                         throw new Exception("not redirected to Assignment Screen");
@@ -156,27 +271,127 @@ namespace SoPro24Team06.E2E
             }
         }
 
-        public async Task TestIfIsRunning(HttpClient client)
+        public async Task SetTestData(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<ApplicationRole> roleManager
+        )
         {
-            bool isRunning = false;
-            for (int i = 0; i < 4; i++)
+            //clear database to ensure an empty database
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            //insert Required Roles:
+            string[] roleNames = { "Administrator", "User", "Personal" };
+            foreach (var roleName in roleNames)
             {
-                try
+                ApplicationRole role = new ApplicationRole(roleName);
+                if (await roleManager.RoleExistsAsync(role.Name) == false)
+                    await roleManager.CreateAsync(role);
+                await context.SaveChangesAsync();
+            }
+
+            //insert Required Users
+            ApplicationUser admin = new ApplicationUser
+            {
+                UserName = "admin@example.com",
+                Email = "admin@example.com",
+                FullName = "Admin User"
+            };
+            if (await userManager.FindByEmailAsync(admin.Email) == null)
+            {
+                await userManager.CreateAsync(admin, "Admin@123");
+                await userManager.AddToRoleAsync(admin, "Administrator");
+            }
+
+            ApplicationUser user = new ApplicationUser
+            {
+                UserName = "user@example.com",
+                Email = "user@example.com",
+                FullName = "User User"
+            };
+            if (await userManager.FindByEmailAsync(user.Email) == null)
+            {
+                await userManager.CreateAsync(user, "User@123");
+                await userManager.AddToRoleAsync(user, "User");
+            }
+            ApplicationUser personal = new ApplicationUser
+            {
+                UserName = "personal@example.com",
+                Email = "personal@example.com",
+                FullName = "Personal User"
+            };
+            if (await userManager.FindByEmailAsync(user.Email) == null)
+            {
+                await userManager.CreateAsync(personal, "Personal@123");
+                await userManager.AddToRoleAsync(personal, "Personal");
+            }
+            //create Deparments and Contracts
+            context.Contracts.Add(new Contract("Test"));
+            context.Departments.Add(new Department("Test"));
+            context.SaveChanges();
+            //create DueTime
+            context.DueTimes.AddRange(
+                new DueTime("dueTime1", 1, 0, 0),
+                new DueTime("dueTime2", 2, 0, 0),
+                new DueTime("dueTime3", -2, 0, 0)
+            );
+            //create Assignments
+            DateTime DueDate = DateTime.Today;
+            List<Assignment> assignmens =
+                new()
                 {
-                    var response = await client.GetAsync("/");
-                    if (response.IsSuccessStatusCode)
+                    new AssignmentTemplate
                     {
-                        isRunning = true;
-                        break;
-                    }
-                }
-                catch { }
-                await Task.Delay(10000);
-            }
-            if (isRunning == false)
+                        Title = "TestRolePersonal",
+                        Instructions = "Test",
+                        AssigneeType = Enums.AssigneeType.ROLES,
+                        AssignedRole = await roleManager.FindByNameAsync("Personal"),
+                        DueIn = context.DueTimes.Find(1),
+                        ProcessTemplateId = 0
+                    }.ToAssignment(personal, DateTime.Today),
+                    new AssignmentTemplate
+                    {
+                        Title = "TestUserPersonal",
+                        Instructions = "Test",
+                        AssigneeType = Enums.AssigneeType.SUPERVISOR,
+                        DueIn = context.DueTimes.Find(2),
+                        ProcessTemplateId = 0
+                    }.ToAssignment(personal, DateTime.Today),
+                    new AssignmentTemplate
+                    {
+                        Title = "TestUserUser",
+                        Instructions = "Test",
+                        AssigneeType = Enums.AssigneeType.WORKER_OF_REF,
+                        DueIn = context.DueTimes.Find(1),
+                        ProcessTemplateId = 0
+                    }.ToAssignment(user, DateTime.Today),
+                    new AssignmentTemplate
+                    {
+                        Title = "TestUserUserOverDue",
+                        Instructions = "Test",
+                        AssigneeType = Enums.AssigneeType.WORKER_OF_REF,
+                        DueIn = context.DueTimes.Find(3),
+                        ProcessTemplateId = 0
+                    }.ToAssignment(user, DateTime.Today)
+                };
+            await context.Assignments.AddRangeAsync(assignmens);
+            await context.SaveChangesAsync();
+
+            List<Process> processes = new List<Process>
             {
-                throw new Exception("client not running");
-            }
+                new Process(
+                    "Test",
+                    "Test",
+                    context.Assignments.ToList(),
+                    user,
+                    personal,
+                    context.Contracts.First(),
+                    context.Departments.First()
+                )
+            };
+            await context.Processes.AddRangeAsync(processes);
+            await context.SaveChangesAsync();
         }
 
         public void Dispose()
