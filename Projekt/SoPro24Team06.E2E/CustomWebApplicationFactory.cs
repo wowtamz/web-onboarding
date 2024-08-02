07a64e117afb82bfc47567d621c8111079accc23
Jan Pfluger <jan.pfluger@uni-a.de>
2024-08-02 17:16:17 +0200

    modified:   SoPro24Team06.E2E/CustomWebApplicationFactory.cs 	modified:   SoPro24Team06/Program.cs 	modified:   ../docs/How To E2E Test with WebApplicationFactory.md

--- Projekt/SoPro24Team06.E2E/CustomWebApplicationFactory.cs
+++ Projekt/SoPro24Team06.E2E/CustomWebApplicationFactory.cs
@@ -37,21 +37,6 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseInMemoryDatabase("TestDatabase")
             );
-            // services.AddSingleton<DbConnection>(container =>
-            // {
-            //     var connection = new SqliteConnection("DataSource=:memory:");
-            //     connection.Open();
-
-            //     return connection;
-            // });
-
-            // services.AddDbContext<ApplicationDbContext>(
-            //     (container, options) =>
-            //     {
-            //         var connection = container.GetRequiredService<DbConnection>();
-            //         options.UseSqlite(connection);
-            //     }
-            // );
         });
     }
 
